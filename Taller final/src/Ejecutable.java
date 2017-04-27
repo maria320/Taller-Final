4e993ea21b18e7083b9ebd5ae0bb8ed761ae1a92
maro <maro@172.30.161.126>
2017-04-27 13:56:08 -0500

    clases y metodos iniciales

---
+++ Taller final/src/Ejecutable.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class Ejecutable extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("Ejecutable");
+
+	}
+
+	public void settings() {
+		size(1200, 700);
+
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+		
+	}
+
+	public void keyPressed() {
+
+	}
+
+}
---
+++ Taller final/src/Logica.java
@@ -0,0 +1,8 @@
+
+public class Logica {
+
+	public Logica() {
+		
+	}
+
+}
